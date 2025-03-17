1981ce371ddc9f9a6ca03fd93aab4c1f342f3c8f
OtavioFidalgo <168548240+OtavioFidalgo@users.noreply.github.com>
2025-03-17 11:53:02 -0300

    Add files via upload

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 
-    namespace PHP\Modelo;
+    /*namespace PHP\Modelo;
     require_once('Cliente.php');
     require_once('Livro.php');
     require_once('Compra.php');
@@ -28,7 +28,26 @@
     echo "<br><br>".$livro1->imprimir();
     echo "<br><br>".$compra1->imprimir();
     echo "<br><br>".$reserva1->imprimir();
-    echo "<br><br>".$login1->imprimir();
+    echo "<br><br>".$login1->imprimir();*/
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/livraria.css"></link>
+</head>
+<body>
+    <div Topo="p-3 mb-2 bg-warning-subtle text-warning-emphasis">Sua Livraria Favorita
+        <br>
+        <h1>Livraria Fidalgo</h1>
+        
+    </div>
+    
+</body>
+</html>
